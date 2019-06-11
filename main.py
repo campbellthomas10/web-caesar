8dcf69f54e394aaf5ebed6555c2230f522727512
Tommy Campbell <tjc2187@gmail.com>
2019-06-11 01:21:54 -0500

    added header and footer

--- main.py
+++ main.py
@@ -11,8 +11,23 @@ form = """<!DOCTYPE html>
     <head>
         <style>
             body {{
+                font-family: Verdana, sans-seriff;
                 background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
             }}
+
+            h1 {{
+                text-align: center;
+                color: white;
+            }}
+
+            footer {{
+                color: white;
+                font-size: 8pt;
+                width: 540px;
+                margin: 0 auto;
+                margin-top: 5px;
+            }}
+
             form {{
                 background-color: #eee;
                 padding: 20px;
@@ -21,6 +36,7 @@ form = """<!DOCTYPE html>
                 font: 16px sans-serif;
                 border-radius: 10px;
             }}
+
             textarea {{
                 margin: 10px 0;
                 width: 540px;
@@ -29,6 +45,7 @@ form = """<!DOCTYPE html>
         </style>
     </head>
     <body>
+      <h1>Caesar Encrypter</h1>
       <form method="POST">
         <label for="rot">Rotate by:</label>
         <input type="text" name="rot" value="0">
@@ -36,8 +53,8 @@ form = """<!DOCTYPE html>
         <textarea name="text">{0}</textarea>
         <br />
         <input type="submit" value="Send">
-
       </form>
+      <footer>By: Tommy Campbell<br />Last Updated: 6/11/19</footer>
     </body>
 </html>"""
 
