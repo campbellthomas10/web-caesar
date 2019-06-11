477b89ea64a97a9abfe227c7bdaf01327596cb12
Tommy Campbell <tjc2187@gmail.com>
2019-06-10 20:19:35 -0500

    added a background gradient

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ form = """<!DOCTYPE html>
 <html>
     <head>
         <style>
+            body {{
+                background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+            }}
             form {{
                 background-color: #eee;
                 padding: 20px;
